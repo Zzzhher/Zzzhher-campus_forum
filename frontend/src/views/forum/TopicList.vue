1a617c3dd41e9155704afe0169d842139d844103
Zzzhher <2142609137@qq.com>
2026-01-05 23:54:34 +0800

    前端将首页帖子广场的布局完成

---
+++ frontend/src/views/forum/TopicList.vue
@@ -0,0 +1,127 @@
+<script setup>
+import LightCard from "@/components/LightCard.vue";
+import {Calendar, CollectionTag, EditPen, Link} from "@element-plus/icons-vue";
+import Weather from "@/components/Weather.vue";
+import { ref, onMounted } from "vue";
+
+const today = ref("");
+
+onMounted(() => {
+  const date = new Date();
+  today.value = `${date.getFullYear()}年${
+    date.getMonth() + 1
+  }月${date.getDate()}日`;
+});
+</script>
+
+<template>
+  <div style="display: flex; margin: 20px auto; gap: 20px; max-width: 900px">
+    <div style="flex: 1">
+      <light-card>
+        <div class="create-topic">
+          <el-icon><EditPen/></el-icon> 点击发表主题...
+        </div>
+      </light-card>
+      <light-card style="margin-top: 10px;height: 30px">
+
+      </light-card>
+      <div style="margin-top: 10px;display: flex;flex-direction: column;gap:10px">
+        <light-card style="height: 150px" v-for="item in 10">
+
+        </light-card>
+      </div>
+    </div>
+
+    <div style="width: 280px">
+      <div style="position: sticky; top: 20px">
+        <light-card>
+          <div style="font-weight: bold">
+            <el-icon><CollectionTag /></el-icon>
+            论坛公告
+          </div>
+          <el-divider style="margin: 10px 0" />
+          <div style="font-size: 14px; margin: 10px; color: grey">
+            为认真学习宣传贯彻党的二十大精神，深入贯彻习近平强军思想，
+            作为迎接办学70周年系列学术活动之一，国防科技大学将于2022年11月24日至26日在长沙举办"国防科技
+          </div>
+        </light-card>
+        <light-card style="margin-top: 10px">
+          <div style="font-weight: bold">
+            <el-icon><Calendar /></el-icon>
+            天气信息
+          </div>
+          <el-divider style="margin: 10px 0" />
+          <weather />
+        </light-card>
+        <light-card style="margin-top: 10px">
+          <div class="info-text">
+            <div>当前日期</div>
+            <div>{{ today }}</div>
+          </div>
+          <div class="info-text">
+            <div>当前IP地址</div>
+            <div>127.0.0.1</div>
+          </div>
+        </light-card>
+        <div style="font-size: 14px; margin-top: 10px; color: grey">
+          <el-icon><Link /></el-icon>
+          友情链接
+        </div>
+        <el-divider style="margin: 10px 0" />
+        <div
+          style="
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 10px;
+            margin-top: 10px;
+          "
+        >
+          <div class="friend-link">
+            <el-image
+              style="height: 100%"
+              src="https://element-plus.org/images/js-design-banner.jpg"
+            />
+          </div>
+          <div class="friend-link">
+            <el-image
+              style="height: 100%"
+              src="https://element-plus.org/images/vform-banner.png"
+            />
+          </div>
+<!--          <div class="friend-link">-->
+<!--            <el-image-->
+<!--              style="height: 100%"-->
+<!--              src="https://element-plus.org/images/sponsors/jnpfsoft.jpg"-->
+<!--            />-->
+<!--          </div>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.info-text {
+  display: flex;
+  justify-content: space-between;
+  color: grey;
+  font-size: 14px;
+}
+.friend-link {
+  border-radius: 5px;
+  overflow: hidden;
+}
+.create-topic {
+  background-color: #efefef;
+  border-radius: 5px;
+  height: 40px;
+  color: grey;
+  font-size: 14px;
+  line-height: 40px;
+  padding: 0 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
